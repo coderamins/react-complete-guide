8d4ccd687a1ea3e593e27838a4c07771b77a232b
1007297 <1007297@1007297T1082753>
2021-06-16 16:14:18 +0430

    Module3-S14 using State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Hi, I'm A Rect App !</h1>
-          <p>This is really working !</p>
-          <Person/>
-      </div>      
+        <h1>Hi, I'm A Rect App !</h1>
+        <p>This is really working !</p>
+        <Person name="Ramin" age="28" />
+        <Person name="Manu" age="29" >My Hobbies : Racing</Person>
+        <Person name="Ali" age="26" />
+      </div>
     );
     ///return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi, I\'m A Rect App !'));
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Person = () => {
+const Person = (props) => {
   return (
-    <p>
-      I'm a Person!
-    </p>
+    <div>
+      <p> I'm a {props.name} and I am {props.age} years old! </p>
+      <b>{props.children}</b>
+    </div>   
+    
   )
 }
 
