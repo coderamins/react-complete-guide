b70b89f65a302d93f2b5986eac4979f34bcf029e
1007297 <1007297@1007297T1082753>
2021-06-26 18:29:52 +0430

    Burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React,{ Component } from "react";
+
+import Aux from '../../hoc/Auxx';
+
+class BurgerBuildser extends Component{
+    render(){
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuildser;
