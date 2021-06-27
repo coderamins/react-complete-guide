1885c6bb71c972e78ab4fa8186023b9150822bb7
1007297 <1007297@1007297T1082753>
2021-06-27 18:25:38 +0430

    Burger App: Start of 15

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,30 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Auxx';
+import Aux from "../../hoc/Auxx";
+import Burger from "../../components/Burger/Burger";
 
-class BurgerBuildser extends Component{
-    render(){
-        return (
-            <Aux>
-                <div>Burger</div>
-                <div>Build Controls</div>
-            </Aux>
-        );
-    }
+class BurgerBuildser extends Component {
+  // constructor(props){
+  //     super(props);
+  //     this.state={...}
+  // }
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 2,
+    },
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuildser;
